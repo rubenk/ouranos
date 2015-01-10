84374ce5f1444e20135b8860220c1a50ca4c319f
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-01-10 22:31:08 +0100

    A few more fixes

--- board/ouranos/post_build.sh
+++ board/ouranos/post_build.sh
@@ -14,3 +14,9 @@ rm -rvf "${target}/usr/lib/rpm/"
 rm -rvf "${target}/usr/share/udhcpc/"
 rm -vf "${target}/usr/bin/kernel-install"
 rm -vf "${target}/usr/lib/libstdc++.so.6.0.20-gdb.py"
+
+# don't build udev hwdb
+rm -vf "${target}/lib/systemd/system/sysinit.target.wants/systemd-udev-hwdb-update.service"
+
+# don't symlink /run to /tmp
+rm -vf "$target}/run" && mkdir "${target}/run"
