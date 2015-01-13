f21260a1b6d4b1ab433f73fa900d8b58eb29dbcf
Ruben Kerkhof <ruben@rubenkerkhof.com>
2015-01-13 18:57:55 +0100

    Use mktemp for tmpdir creation

--- board/ouranos/post_image.sh
+++ board/ouranos/post_image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 IMG=${BINARIES_DIR}/rootfs.ext4
+TMPDIR=$(mktemp -d)
 
 # TODO: do this without sudo
-sudo mkdir -p /tmp/bla
-sudo mount -o loop ${IMG} /tmp/bla
-sudo ${HOST_DIR}/sbin/extlinux -i /tmp/bla/boot/syslinux/
-sudo umount /tmp/bla
-sudo rmdir /tmp/bla
+sudo mount -o loop ${IMG} ${TMPDIR} && \
+	sudo ${HOST_DIR}/sbin/extlinux -i ${TMPDIR}/boot/syslinux/ && \
+	sudo umount ${TMPDIR} && \
+	sudo rmdir ${TMPDIR}
